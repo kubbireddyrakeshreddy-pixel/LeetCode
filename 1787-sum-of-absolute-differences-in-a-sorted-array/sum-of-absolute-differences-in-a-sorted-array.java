14e5fe8c48d8a30b0b9a0ddb6378866c7f95e205
kubbireddyrakeshreddy-pixel <kubbireddyrakeshreddy@gmail.com>
2026-02-05 20:21:40 +0530

    Time: 4 ms (82.99%) | Memory: 85.3 MB (90.38%) - LeetSync

---
+++ 1787-sum-of-absolute-differences-in-a-sorted-array/sum-of-absolute-differences-in-a-sorted-array.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] getSumAbsoluteDifferences(int[] nums) {
+        int n = nums.length;
+        int[] res = new int[n];
+        int total = 0;
+        for (int x : nums) total += x;
+
+        int prefix = 0;
+        for (int i = 0; i < n; i++) {
+            int left = nums[i] * i - prefix;
+            int right = (total - prefix - nums[i]) - nums[i] * (n - i - 1);
+            res[i] = left + right;
+            prefix += nums[i];
+        }
+        return res;
+    }
+}
