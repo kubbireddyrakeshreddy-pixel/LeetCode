ded22c672adab1389b87d1cc9498883d8542e6e0
kubbireddyrakeshreddy-pixel <kubbireddyrakeshreddy@gmail.com>
2026-02-20 16:52:47 +0530

    Time: 5 ms (42.21%) | Memory: 45.1 MB (45.00%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        StringBuilder sb=new StringBuilder();
+        for(char c:s.toCharArray()){
+            if(Character.isLetterOrDigit(c)){
+                sb.append(c);
+            }
+        }
+       sb= new StringBuilder((sb.toString()).toLowerCase());
+        int j=sb.length()-1;
+        int i=0;
+        while(i<=j){
+            if(sb.charAt(i)!=sb.charAt(j)){
+                return false;
+            }
+            j--;
+            i++;
+            
+        }
+return true;
+        }
+}
+        
+    
