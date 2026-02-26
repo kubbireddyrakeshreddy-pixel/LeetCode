beea366164ccb67c96b15590bd8f27082f29657f
kubbireddyrakeshreddy-pixel <kubbireddyrakeshreddy@gmail.com>
2026-02-26 06:52:49 +0530

    Time: 16 ms (38.85%) | Memory: 48 MB (14.58%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -15,7 +15,7 @@ class Solution {
             }
         }
         
-        for(int i = k - 1; i >= 0; i--){
+        for(int i = 0; i <k; i++){
             arr[i] = min.poll();
         }
         
