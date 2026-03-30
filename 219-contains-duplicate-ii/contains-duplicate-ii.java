24ee80628a8c78899d3e8332b4f6da906af579d1
kubbireddyrakeshreddy-pixel <kubbireddyrakeshreddy@gmail.com>
2026-03-30 11:50:59 +0530

    Time: 24 ms (55.07%) | Memory: 76.1 MB (87.71%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.java
+++ 219-contains-duplicate-ii/contains-duplicate-ii.java
@@ -1,17 +1,16 @@
 class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
-       
-HashMap<Integer,Integer> map =new HashMap<>();
+HashSet<Integer> set=new HashSet<>();
 for(int i=0;i<nums.length;i++){
-    if(map.containsKey(nums[i])&&i-map.get(nums[i])<=k){
+      if(set.contains(nums[i])){
         return true;
-
-    }
-    map.put(nums[i],i);
-
+      }
+      set.add(nums[i]);
+     if(set.size()>k){
+        set.remove(nums[i-k]);
+     }
 }
+
 return false;
-
-
 
     }}
